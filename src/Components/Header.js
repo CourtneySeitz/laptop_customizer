f25d94ae00cdf070fda9a5109560dd619cd0d324
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2021-01-14 14:39:50 -0500

    cleaning up imports

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
+
 export default class Header extends React.Component {
-    render() {
-        return (
+render () {
+    return (
         <header>
             <h1>ELF Computing | Laptops</h1>
-            </header>  
-        );
+        </header>
+    )
 }
 }
