b3ae9289433cb39fd23034e6630b4cccbf31018b
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2021-01-15 16:13:52 -0500

    small changes

--- src/Components/MainForm.js
+++ src/Components/MainForm.js
@@ -25,7 +25,6 @@ export default class MainForm extends React.Component {
                 <fieldset className="feature" key={featureHash}>
                     <legend className="feature__name">
                     <h3>{feature}</h3>
-                
                     </legend>
                     {options}
                 </fieldset>
